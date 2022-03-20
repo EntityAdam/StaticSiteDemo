c47dd9cddb248dabd7b1acf10743c7ceb7331f37
Adam Vincent <adam@entityadam.com>
2022-03-20 04:54:00 -0400

    changed azure region to eastus2

--- inf/MyStackConfiguration.cs
+++ inf/MyStackConfiguration.cs
@@ -6,7 +6,7 @@ public class MyStackConfiguration
     public string ResourceGroupName => $"{Organization}-{Application}-{Environment}-rg";
     public string StaticSiteName => $"{Organization}-{Application}-{Environment}-web";
     public string Branch { get; set; } = "main";
-    public string Location { get; set; } = "eastus";
+    public string Location { get; set; } = "eastus2";
     public string FunctionsFolder { get; set; } = "Functions";
     public string AppFolder { get; set; } = @"..\src\Client";
     public string AppBuildFolder { get; set; } = @"..\src\Client\bin\release\net6.0";

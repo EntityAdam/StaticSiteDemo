1f391419495404f01404721d5e8fe3e00cc66b36
Adam Vincent <adam@entityadam.com>
2022-03-21 15:12:10 -0400

    resolved path separator

--- inf/MyStackConfiguration.cs
+++ inf/MyStackConfiguration.cs
@@ -9,8 +9,8 @@ public class MyStackConfiguration
     public string TenantId => "b848e0f6-95bd-4f24-a1dd-3b6c3c7813e1";
     public string Branch { get; set; } = "main";
     public string Location { get; set; } = "eastus2";
-    public string FunctionsFolder { get; set; } = @"src\Functions";
-    public string AppFolder { get; set; } = @"src\Client";
+    public string FunctionsFolder { get; set; } = @"src/Functions";
+    public string AppFolder { get; set; } = @"src/Client";
     //public string AppBuildFolder { get; set; } = @"src\Client\bin\release\net6.0";
     public string RepositoryUrl { get; set; } = "https://github.com/entityadam/StaticSiteDemo";
     public string? RepositoryToken { get; set; }
